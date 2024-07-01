34e1a36e2412ea64887d3ef9b9ad21609c808dc3
adrs1166ma <adrsnego16@gmail.com>
2024-07-01 16:03:49 -0500

    parte 10-I

--- miapp/models.py
+++ miapp/models.py
@@ -2,17 +2,28 @@ from django.db import models
 
 # Create your models here.
 class Articulo(models.Model):
-    titulo = models.CharField(max_length=150)
-    contenido = models.TextField()
-    imagen = models.ImageField(default='null')
-    publicado = models.BooleanField()
-    # auto_now_add me permitirá registrar 
-    # la fecha cuando cree el registro
-    creado = models.DateTimeField(auto_now_add=True)
-    # auto_now me permitirá registrar la fecha 
-    # cuando se modifique el registro
-    actualizado = models.DateTimeField(auto_now=True)
+    titulo = models.CharField(max_length=150, verbose_name="Título")
+    contenido = models.TextField(verbose_name="Contenido")
+    imagen = models.ImageField(default='null', verbose_name="Miniatura", upload_to="articulos")
+    publicado = models.BooleanField(verbose_name="¿Publicado?")
+    # auto_now_add=True me pertmite 
+    # registrar la fecha de creación del registro
+    creado = models.DateTimeField(auto_now_add=True, verbose_name="Creado")
+    # auto_now=True me permite registrar
+    # la fecha cuando se modifique el registro
+    actualizado = models.DateTimeField(auto_now=True, verbose_name="Editado")
 
+    class Meta:
+        verbose_name = "Artículo"
+        verbose_name_plural = "Artículos"        
+        ordering = ['-titulo', 'publicado'] 
+
+        def __str__(self):
+            if self.publicado:
+                publicado = "Publicado"
+            else:
+                publicado = "No Publicado / En Revisión"
+            return f"{self.titulo}. Creado el:{self.creado}. Publicado: {self.publicado}"
 
 class Categoria(models.Model):
     nombre = models.CharField(max_length=110)
@@ -20,3 +31,7 @@ class Categoria(models.Model):
     # DateField() para guardar la fecha manualmente
     creado = models.DateField()
 
+    class Meta:
+        verbose_name = "Categoría"
+        verbose_name_plural = "Categorías"
+        ordering = ['-titulo']
