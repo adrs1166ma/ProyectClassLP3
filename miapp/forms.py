dcf0112f2436846853e09da1568e7738307a16a3
adrs1166ma <163654808+adrs1166ma@users.noreply.github.com>
2024-07-01 13:02:44 -0500

    parte 9 completa

---
+++ miapp/forms.py
@@ -0,0 +1,27 @@
+from django import forms
+from django.core import validators
+class FormArticulo(forms.Form):
+    titulo = forms.CharField(
+        label="Titulo",
+        max_length=40,
+        required=True,
+        widget=forms.TextInput(
+            attrs={
+                'placeholder': 'Ingrese el título',
+                'class': 'titulo_form_articulo'
+            }
+        ),
+        validators=[
+            validators.MinLengthValidator(4, 'El título es corto'),
+            validators.RegexValidator('^[A-Za-z0-9]*$','El título tiene caracteres inválidos','titulo_invalido')            
+        ]    
+    
+    )
+    contenido = forms.CharField(
+        label = "Contenido",
+        widget = forms.Textarea,
+        validators=[
+            validators.MaxLengthValidator(20,'Superaste el límite de caracteres')
+        ]
+    )
+
--- miapp/views.py
+++ miapp/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, HttpResponse, redirect
-
+from miapp.models import Articulo
+from miapp.forms import FormArticulo
+from django.contrib import messages
 # Create your views here.
 
 
@@ -90,4 +92,109 @@ def rango(request):
         'a':a,
         'b':b,
         'rango_numeros':rango_numeros
+    })
+
+
+
+
+def crear_articulo(request,titulo, contenido, publicado):
+    articulo = Articulo(
+        titulo = titulo,
+        contenido = contenido,
+        publicado = publicado
+    )
+    articulo.save()
+    return HttpResponse(f"Articulo Creado: {articulo.titulo} - {articulo.contenido}")
+
+def buscar_articulo(request):
+    try:
+        articulo = Articulo.objects.get(id=1000)
+        resultado = f"""Articulo: 
+                        <br> <strong>ID:</strong> {articulo.id} 
+                        <br> <strong>Título:</strong> {articulo.titulo} 
+                        <br> <strong>Contenido:</strong> {articulo.contenido}
+                        """
+    except:
+        resultado = "<h1> Artículo No Encontrado </h1>"
+    return HttpResponse(resultado)
+
+
+def editar_articulo(request, id):
+    articulo = Articulo.objects.get(pk=id)
+
+
+    articulo.titulo = "Enseñanza onLine en la UNTELS"
+    articulo.contenido = "Aula Virtual, Google Meet, Portal Académico, Google Classroom..."
+    articulo.publicado = False
+
+
+    articulo.save()
+    return HttpResponse(f"Articulo Editado: {articulo.titulo} - {articulo.contenido}")
+
+def listar_articulos(request):
+    articulos = Articulo.objects.all();
+    """articulos = Articulo.objects.filter(
+        Q(titulo__contains="Py") |
+        Q(titulo__contains="Hab")
+    )"""
+    return render(request, 'listar_articulos.html',{
+        'articulos': articulos,
+        'titulo': 'Listado de Artículos'
+    })
+
+    
+def eliminar_articulo(request, id):
+    articulo = Articulo.objects.get(pk=id)
+    articulo.delete()
+    return redirect('listar_articulos')
+
+def save_articulo(request):
+    if request.method == 'POST':
+        titulo = request.GET['titulo']
+        if len(titulo)<=5:
+            return HttpResponse("<h2>El tamaño del título es pequeño, intente nuevamente</h2>")
+        contenido = request.POST['contenido']
+        publicado = request.POST['publicado']
+
+
+        articulo = Articulo(
+            titulo = titulo,
+            contenido = contenido,
+            publicado = publicado
+        )
+        articulo.save()
+        return HttpResponse(f"Articulo Creado: {articulo.titulo} - {articulo.contenido}")
+    else:
+        return HttpResponse("<h2>No se ha podido registrar el artículo</h2>")
+
+def create_articulo(request):
+    return render(request, 'create_articulo.html')
+
+def create_full_articulo(request):
+    if request.method == 'POST':
+        formulario = FormArticulo(request.POST)
+        if formulario.is_valid():
+            data_form = formulario.cleaned_data
+            # Hay 2 formar de recuperar la información
+            titulo  = data_form.get('titulo')
+            contenido = data_form['contenido']
+            publicado = data_form['publicado']
+            articulo = Articulo(
+                titulo = titulo,
+                contenido = contenido,
+                publicado = publicado
+            )
+            articulo.save()
+            # Crear un mensaje flash (Sesión que solo se muestra 1 vez)
+            messages.success(request, f'Se agregó correctamente el artículo {articulo.id}')
+            return redirect('listar_articulos')
+            #return HttpResponse(articulo.titulo + ' -  ' + articulo.contenido + ' - ' + str(articulo.publicado))
+
+    else:
+        formulario = FormArticulo()
+        # Generamos un formulario vacío
+
+
+    return render(request, 'create_full_articulo.html',{
+        'form': formulario
     })
